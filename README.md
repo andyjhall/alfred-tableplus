7daaaa86eec3d487694fa6da265cbe8aec09e925
Andy Hall <andrewhall@cellhire.com>
2022-05-10 17:11:13 +0100

    init

---
+++ README.md
@@ -0,0 +1,16 @@
+# Alfred TablePlus Connections
+
+Quickly navigate to TablePlus connections in [Alfred 3][alfred].
+
+![][sample]
+
+
+# Thanks, License, Copyright
+- The [Alfred-Workflow][alfred-workflow] library which is heavily used.
+- The TablePlus icon is used, care of TablePlus.
+
+All other code/media are released under the MIT Licence.
+
+[alfred]: http://www.alfredapp.com/
+[alfred-workflow]: http://www.deanishe.net/alfred-workflow/
+[sample]: https://raw.github.com/andyjhall/alfred-tableplus/main/docs/sample.png
---
+++ src/ConnectionSearcher.py
@@ -0,0 +1,53 @@
+#!/usr/local/bin/python3
+# -*- coding: utf-8 -*-
+
+import os
+import json
+import sys
+import glob
+import plistlib
+from workflow import Workflow3, ICON_WARNING, MATCH_SUBSTRING
+
+def read_connections():
+    pl = plistlib.readPlist(os.environ["HOME"] + "/Library/Application Support/com.tinyapp.TablePlus/Data/Connections.plist")
+    connections = []
+    i = 0
+    while i < len(pl):
+        connections.append({
+                    'id': pl[i]['ID'],
+                    'name': pl[i]['ConnectionName'],
+                    'host': pl[i]['DatabaseHost']
+                })
+        i += 1
+    return connections
+
+
+
+def filter_key_for_connection(connection):
+    return connection['name'] + ' ' + str(connection['id'])
+
+
+def sort_key_for_connection(connection):
+    return connection['name']
+
+def main(wf):
+    query = wf.args[0]
+    connections = wf.cached_data('connections', read_connections, max_age=30)
+    
+    if query and connections:
+        connections = wf.filter(query, connections, filter_key_for_connection, match_on=MATCH_SUBSTRING)
+    
+    connections.sort(key=sort_key_for_connection)
+    for connection in connections:
+        wf.add_item(title=connection['name'],
+                    subtitle=connection['host'],
+                    arg='tableplus://?id='+connection['id'],
+                    valid=True)
+
+    wf.send_feedback()
+
+
+if __name__ == u"__main__":
+    wf = Workflow3()
+    log = wf.logger
+    sys.exit(wf.run(main))
